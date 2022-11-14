450b4998f9535e969f3ac95bf6ef543970671cbf
user <user@218.154.25.42>
2022-11-15 00:20:17 +0900

    실수

---
+++ day01/src/day01/Float.java
@@ -0,0 +1,16 @@
+package day01;
+
+public class Float {
+	public static void main(String[] args) {
+		float a = 1.5f;
+		double b = 1.5;
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+		float c = 1.5145458741512155454f;
+		
+		System.out.println(c);
+	}
+
+}
