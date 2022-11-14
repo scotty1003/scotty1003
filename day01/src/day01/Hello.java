6ca006cc7a8fe7fc8146ad02b04e1b7b988f0eb3
user <user@218.154.25.42>
2022-11-15 00:06:05 +0900

    day 01 생성

---
+++ day01/src/day01/Hello.java
@@ -0,0 +1,12 @@
+package day01;
+
+//import는 준비 명령
+import java.lang.*;
+public class Hello {
+	public static void main(String[] args) {
+		//출력 코드 - 결과화면에 원하는 글자를 출력
+		System.out.println("Hello java!");
+		System.out.println("Hello java!");
+		//System.out.println("Hello java!"); 
+	} 
+}
