f051078c93bc4671da659fadabdf16c5ac6cd9dd
user <user@218.154.25.42>
2022-11-15 00:45:49 +0900

    논리적

---
+++ day01/src/day01/Boolean.java
@@ -0,0 +1,15 @@
+package day01;
+
+public class Boolean {
+	public static void main(String[] args) {
+		boolean a = true;
+		System.out.println(a);
+		
+		int money = 20000;
+		int pizza = 25000;
+		
+		boolean order = money >= pizza;
+		System.out.println(order);
+	}
+
+}
