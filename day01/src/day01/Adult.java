73125e6493e642a69b82cc0e34d04491dc054cf9
user <user@218.154.25.42>
2022-11-15 00:43:46 +0900

    성인 판정

---
+++ day01/src/day01/Adult.java
@@ -0,0 +1,16 @@
+package day01;
+
+public class Adult {
+	public static void main(String[] args) {
+		
+		
+		int year = 2000;
+		int now = 2022;
+		
+		int age = now - year + 1;
+		
+		boolean adult = age >= 20;
+		System.out.println(adult);
+		
+	}
+}
