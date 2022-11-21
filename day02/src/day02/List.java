879d5bb04d01d477a816cb3ee0f1aefa0c8dc0a6
scotty1003 <scotty1003@naver.com>
2022-11-21 23:55:25 +0900

    배열로 성적 처리 관련 문제 풀이

---
+++ day02/src/day02/List.java
@@ -0,0 +1,40 @@
+package day02;
+
+public class List {
+	public static void main(String[] args) {
+		int[] score = new int[] {75, 92, 86, 65, 79, 95, 88, 86, 72, 77};
+		
+		
+		//1
+		for (int i =0; i < score.length; i++) {
+			System.out.println(score[i]);
+		}
+		
+		//2
+		int total = 0;
+		double avg;
+		for (int i =0; i < score.length; i++) {
+			total += score[i];
+		}
+		avg = (double)total / score.length;
+		System.out.println(avg);
+		
+		//3
+		int count=0;
+		for (int i =0; i < score.length; i++) {
+			if(score[i] >= 90) {
+				count ++;
+			}
+		}
+		System.out.println(count);
+		
+		//4
+		int rank = 0;
+		for (int i =0; i < score.length; i++) {
+			if(score[i] >= 85) {
+				rank ++;
+			}
+		}
+		System.out.println(rank);
+	}
+}
